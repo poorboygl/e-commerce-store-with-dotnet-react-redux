11289190b597da7c164fe9d399c6b4ef6d019c51
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-12-24 19:48:58 +0700

    ch02-007 Creating the Entity Framework DbContext class

--- Restore/API/Program.cs
+++ Restore/API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
