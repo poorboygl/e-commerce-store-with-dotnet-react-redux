5a8074816863d59b0a17ca2df76d4a7629825824
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2026-01-03 05:19:06 +0700

    ch03-009 Adding a Typescript type for the Product

--- Restore/client/src/App.tsx
+++ Restore/client/src/App.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
+import type { Product } from "./assets/product";
 
 function App() {
-  const [products, setProducts] = useState<{ name: string; price: number }[]>(
-    []
-  );
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -15,8 +14,14 @@ function App() {
     setProducts((prevState) => [
       ...prevState,
       {
+        id: prevState.length + 1,
         name: "product" + (prevState.length + 1),
         price: prevState.length * 100 + 100,
+        quantityInStock: 100,
+        description: "test",
+        pictureUrl: "https://picsum.photo/200",
+        type: 'test',
+        brand: 'test'
       },
     ]);
   };
